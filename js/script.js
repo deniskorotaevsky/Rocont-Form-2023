933156300ee99eb62ff0958d332ba00c5c331233
Denis Korotaevsky <huligan8771@gmail.com>
2023-10-11 23:40:50 +0300

    validateInputs

--- js/script.js
+++ js/script.js
@@ -24,6 +24,12 @@ const setSuccess = (element) => {
     inputControl.classList.remove('error');
 };
 
+function checkValidity(event) {
+    const formNode = event.target.form
+    const isValid = formNode.checkValidity()
+    formNode.querySelector('button').disabled = !isValid
+}
+
 const validateInputs = () => {
     const usernameValue = username.value.trim();
     const surnameValue = surname.value.trim();
@@ -74,9 +80,9 @@ fileInput.addEventListener('change', function () {
 
 function deleteCurrentFile(thisElement) {
     let currentIndex = thisElement.id;
-    let crop = currentIndex.split('_')[1];
-    if (crop <= comment.length) {
-        comment.splice(crop, 1)
+    let el = currentIndex.split('_')[1];
+    if (el <= comment.length) {
+        comment.splice(el, 1)
     }
     draw()
 }
@@ -86,10 +92,14 @@ function draw() {
     for (let i = 0; i < comment.length; i++) {
         out += `
         <div class="input-file-li_row" id="buttonBusket_${i}" onclick="deleteCurrentFile(this)">
-                <img src="./img/basket.png" />
+                <img src="./resources/basket.png" />
                 <li>${comment[i]}</li>
         </div>
         `
     }
     fileList.innerHTML = out
 };
+
+form.addEventListener('submit', validateInputs)
+form.addEventListener('input', checkValidity)
+form.querySelector('button').disabled = true;
